d5717eec40ad60927d4f687727735657e914e58c
sumit <sumit79sahu@gmail.com>
2025-12-18 18:05:58 +0530

    redux configuration

--- utils/provider.tsx
+++ utils/provider.tsx
@@ -1,11 +1,20 @@
+"use client";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ConfigProvider } from "antd";
-import React from "react";
+import React, { useRef } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { AppStore, makeStore } from "../lib/store";
 
 const Provider = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  const storeRef = useRef<AppStore>(null);
+  if (storeRef.current === null) {
+    storeRef.current = makeStore();
+  }
   return (
     <AntdRegistry>
-      <ConfigProvider >{children}</ConfigProvider>
+      <ConfigProvider>
+        <ReduxProvider store={storeRef.current}>{children}</ReduxProvider>
+      </ConfigProvider>
     </AntdRegistry>
   );
 };
